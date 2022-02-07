33a4af58ccd40c45be5d60d6d6b6b95bbc5fdab1
Gaia Galhardo <ti.gaiagalhardo@gmail.com>
2022-02-07 01:16:03 -0300

    refact: refatorando os serviços de fornecedora

--- src/sdk/service/Fornecedora.service.ts
+++ src/sdk/service/Fornecedora.service.ts
@@ -9,11 +9,24 @@ class FornecedoraService extends Service {
             .then(this.getData)
     }
 
+    static getDetailedFornecedora(fornecedoraId: number) {
+        return this.Http
+            .get<Fornecedora.Detailed>(`/fornecedoras/${fornecedoraId}`)
+            .then(this.getData)
+    }
+
     static insertNewFornecedora(fornecedora: Fornecedora.Input) {
         return this.Http
             .post<Fornecedora.Summary>('/fornecedoras', fornecedora)
             .then(this.getData)
     }
+
+    static updateExistingFornecedora(fornecedoraId: number, fornecedoraData: Fornecedora.Input) {
+        return this.Http
+            .put<Fornecedora.Detailed>(`/fornecedoras/${fornecedoraId}`, fornecedoraData)
+            .then(this.getData)
+    }
+
 
 }
 
